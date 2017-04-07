7e1191578ee56ed7c2f2fda1d5aa75d184c469b0
xinboxu16 <xinboxu@126.com>
2017-04-07 18:40:59 +0800

    修改

--- Assets/Scripts/UI/Login/Login.cs
+++ Assets/Scripts/UI/Login/Login.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using uTools;
 
 public class Login : MonoBehaviour {
 
@@ -54,9 +55,20 @@ public class Login : MonoBehaviour {
         if (obj != null) m_EventList.Add(obj);
         obj = LogicSystem.EventChannelForGfx.Subscribe("ge_ui_unsubscribe", "ui", UnSubscribe);
         if (obj != null) m_EventList.Add(obj);
+
         //获取上一次登录的服务器ID，若没有则默认值为0
         m_ServerId = PlayerPrefs.GetInt("LastLoginServerId");
         SetCurrentServer(m_ServerId);
+
+        m_LogicServerId = PlayerPrefs.GetInt("LastLoginLogicServerId");
+        SetCurrentLogicServer(m_LogicServerId);
+
+#if UNITY_IPHONE
+        CYMGWrapperIOS.GetUniqueIdentifier();
+#else
+        string deviceUniqueIdentifier = ((uint)Application.dataPath.GetHashCode()).ToString();
+        LogicSystem.PublishLogicEvent("ge_device_info", "lobby", deviceUniqueIdentifier);
+#endif
     }
 
     //设置当前服务器
@@ -82,6 +94,153 @@ public class Login : MonoBehaviour {
         catch (Exception ex)
         {
             DashFire.LogicSystem.LogicLog("[Error]:Exception:{0}\n{1}", ex.Message, ex.StackTrace);
+        }
+    }
+
+    public void SetCurrentLogicServer(int serverId)
+    {
+        try
+        {
+            m_LogicServerId = serverId;
+        }
+        catch(Exception ex)
+        {
+            LogicSystem.LogicLog("[Error]:Exception:{0}\n{1}", ex.Message, ex.StackTrace);
+        }
+    }
+
+    public void OnLoginResult(int result, string accountId)
+    {
+        try
+        {
+            AccountLoginResult ret = (AccountLoginResult)result;
+            if (ret == AccountLoginResult.Success)
+            {
+                LogicSystem.EventChannelForGfx.Publish("ge_ui_connect_hint", "ui", true, false);
+                //登录成功  
+                UIManager.Instance.HideWindowByName("LoadingHint");
+                if (CYMGAvailable)
+                {
+#if UNITY_IPHONE
+                    CYMGWrapperIOS.OnLoginBillingSuccess(accountId);
+#endif
+                }
+            }else if(ret == AccountLoginResult.FirstLogin)
+            { 
+                LogicSystem.EventChannelForGfx.Publish("ge_ui_connect_hint", "ui", true, false);
+                //账号首次登录，提示输入激活码 
+                UIManager.Instance.HideWindowByName("LoginPrefab");
+                UIManager.Instance.ShowWindowByName("Verification");
+                UIManager.Instance.HideWindowByName("LoadingHint");
+                //UISkillGuide.Instance.SetSteps(1);//第一次登陆初始技能教学预设 未实现
+                if (CYMGAvailable)
+                {
+#if UNITY_IPHONE
+                    CYMGWrapperIOS.OnLoginBillingSuccess(accountId);
+#endif
+                }
+                else
+                {
+                    //登录失败,重新登录
+                    Debug.LogError("Account verify failed...Try again.");
+                    if (CYMGAvailable)
+                    {
+#if UNITY_IPHONE
+                         CYMGWrapperIOS.StartLogin(false);
+#endif
+                    }
+                }
+
+            }
+        }
+        catch(Exception e)
+        {
+            LogicSystem.LogicLog("[Error]:Exception:{0}\n{1}", e.Message, e.StackTrace);
+        }
+    }
+
+    public void OnRoleListResult(bool isSuccess)
+    {
+        try
+        {
+            if(isSuccess == true)
+            {
+                //获取角色列表成功，跳转到角色选择页面
+                UIManager.Instance.HideWindowByName("LoginPrefab");
+                LogicSystem.EventChannelForGfx.Publish("ge_create_hero_scene", "ui", true);
+            }
+            else
+            {
+                //获取角色列表失败，给出错误提示
+            }
+        }
+        catch (Exception ex)
+        {
+            LogicSystem.LogicLog("[Error]:Exception:{0}\n{1}", ex.Message, ex.StackTrace);
+        }
+    }
+
+    public void OnLoginButtonClick()
+    {
+        ServerConfig serverConfig = ServerConfigProvider.Instance.GetDataById(m_ServerId);
+        if(serverConfig == null)
+        {
+            Debug.LogError("Can't read server info!!!");
+            return;
+        }
+
+        PlayerPrefs.SetInt("LastLoginServerId", m_ServerId);
+        m_LogicServerId = serverConfig.LogicServerId;
+        PlayerPrefs.SetInt("LastLoginLogicServerId", m_LogicServerId);
+        string nodeIp = serverConfig.NodeIp;
+        int nodePort = serverConfig.NodePort;
+        string serverAddress = "ws://" + nodeIp + ":" + nodePort;
+        LogicSystem.PublishLogicEvent("ge_select_server", "lobby", serverAddress, m_LogicServerId);
+        if (CYMGAvailable)
+        {
+            //连接畅游平台SDK，账号登录
+#if UNITY_IPHONE
+            CYMGWrapperIOS.StartLogin(true);
+#else
+            LogicSystem.EventChannelForGfx.Publish("ge_ui_connect_hint", "ui", true, true);
+            LogicSystem.PublishLogicEvent("ge_direct_login", "lobby");
+#endif
+        }
+        else
+        {
+            //无账号登录，快速开始
+            LogicSystem.EventChannelForGfx.Publish("ge_ui_connect_hint", "ui", true, true);
+            LogicSystem.PublishLogicEvent("ge_direct_login", "lobby");
+        }
+    }
+
+    public void OnChangeServer()
+    {
+        UIManager.Instance.ShowWindowByName("ServerSelect");
+        LogicSystem.EventChannelForGfx.Publish("ge_recent_server", "ui", m_ServerId);
+        if (goServerBtn != null)
+        {
+            Button uiBtn = goServerBtn.GetComponent<Button>();
+            if (uiBtn != null) uiBtn.enabled = false;
+            Vector3 serverBtnPos = goServerBtn.transform.localPosition;
+            TweenPosition tweenPos = TweenPosition.Begin(goServerBtn, goServerBtn.transform.position, new Vector3(serverBtnPos.x, serverBtnPos.y + TweenOffset, 0.0f), DurationForUpwards);
+            if (tweenPos != null)
+            {
+                tweenPos.animationCurve = CurveForUpwards;
+                tweenPos.onFinished.AddListener(OnTweenUpwardsFinished);
+            }
+        }
+    }
+
+    private void OnTweenUpwardsFinished()
+    {
+        if (goServerBtn != null)
+        {
+            Button uibtn = goServerBtn.GetComponent<Button>();
+            if (uibtn != null) uibtn.enabled = true;
+            TweenPosition tween = goServerBtn.GetComponent<TweenPosition>();
+            if (tween != null) Destroy(tween);
+            NGUITools.SetActive(goServerBtn, false);
         }
     }
 }
